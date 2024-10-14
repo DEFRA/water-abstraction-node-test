c72962d418b238e4146c63048981b7913ee866c9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-10-14 14:51:45 +0100

    Add example of simple mocking (#17)
    We've picked `CheckBusyBillRunsService`, a service in [water-abstraction-system](https://github.com/DEFRA/water-abstraction-system), as our first candidate to demonstrate mocking.
    
    It relies on our `db/db.js` module to make a raw query to the DB. In **water-abstraction-system**, we're able to mock out the call to `select()` and, therefore, easily provide the data `CheckBusyBillRunsService` needs to exercise its logic.
    
    We wanted to try and replicate this simple example in node-test runner, and we have!

---
+++ specs/services/bill-runs/check-busy-bill-runs.service.test.js
@@ -0,0 +1,71 @@
+// Test framework dependencies
+import { describe, it, beforeEach, afterEach, mock } from 'node:test'
+import assert from 'node:assert/strict'
+
+// Things we need to stub
+import { db } from '../../../db/db.js'
+
+// Thing under test
+import CheckBusyBillRunsService from '../../../app/services/bill-runs/check-busy-bill-runs.service.js'
+
+describe('Check Busy Bill Runs service', () => {
+  afterEach(() => {
+    mock.reset()
+  })
+
+  describe('when there are both building and cancelling bill runs', () => {
+    beforeEach(() => {
+      mock.method(db, 'select', async () => {
+        return [{ cancelling: true, building: true }]
+      })
+    })
+
+    it('returns "both"', async () => {
+      const result = await CheckBusyBillRunsService()
+
+      assert.equal(result, 'both')
+    })
+  })
+
+  describe('when there are cancelling bill runs', () => {
+    beforeEach(() => {
+      mock.method(db, 'select', async () => {
+        return [{ cancelling: true, building: false }]
+      })
+    })
+
+    it('returns "cancelling"', async () => {
+      const result = await CheckBusyBillRunsService()
+
+      assert.equal(result, 'cancelling')
+    })
+  })
+
+  describe('when there are building bill runs', () => {
+    beforeEach(() => {
+      mock.method(db, 'select', async () => {
+        return [{ cancelling: false, building: true }]
+      })
+    })
+
+    it('returns "building"', async () => {
+      const result = await CheckBusyBillRunsService()
+
+      assert.equal(result, 'building')
+    })
+  })
+
+  describe('when there are no building or cancelling bill runs', () => {
+    beforeEach(() => {
+      mock.method(db, 'select', async () => {
+        return [{ cancelling: false, building: false }]
+      })
+    })
+
+    it('returns "none"', async () => {
+      const result = await CheckBusyBillRunsService()
+
+      assert.equal(result, 'none')
+    })
+  })
+})
